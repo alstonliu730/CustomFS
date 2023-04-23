6755eca9ae6fc7349964559294adbf177d6afe6d
Alston Liu <alstonliu730@gmail.com>
2023-04-23 12:33:55 -0400

    Deleted assertion for start of path

--- directory.c
+++ directory.c
@@ -157,8 +157,6 @@ void print_directory(inode_t *dd) {
 
 // get inode from the given path
 int get_inode_path(const char* path) {
-    assert(path[0] == '/');
-    
     if(strcmp(path, "/") == 0) {
         printf("DEBUG: get_inode_path(%s) -> returned root inum (%i)\n", path, nROOT);
         return nROOT;
