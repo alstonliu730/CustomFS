d4fd6944f5ab84d12d9d786cbfffe15700a9a217
Alston Liu <alstonliu730@gmail.com>
2023-04-24 21:25:15 -0400

    Fix bug where get_parent doesn't add a slash for the root

--- storage.c
+++ storage.c
@@ -306,12 +306,13 @@ void get_parent(const char *path, char* str) {
     str[0] = '\0';
     // the case where the first string is a slash
     if(!strcmp(copy->data, "") && copy->next) {
+        strcat(str, "/"); // will always add the root
         copy = copy->next; // go to the next case
     }
 
     while (copy->next) {
         printf("DEBUG: get_parent(%s) -> str: %s\n", path, str);
-        strcat(str, "/"); // will always add the root
+        strcat(str, "/"); 
         strncat(str, copy->data, strlen(copy->data) + 1);
         copy = copy->next;
     }
